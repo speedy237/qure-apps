83ed0bfb753b0053a6da40e8cf5900cca0ae47db
Louis Jordan YIYUEME TEYOU <ljyiyuemeteyou@iliad-free.fr>
2024-07-13 23:50:05 +0200

    nothing change

--- qureapi/settings.py
+++ qureapi/settings.py
@@ -132,3 +132,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
